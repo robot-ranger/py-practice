3a1b383fa6bb6010b22ab70b31cd925bb0f53c72
ted <ted@roguebuckmechanical.com>
2022-01-01 20:06:06 -0800

    init

---
+++ PID.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from simple_pid import PID
+
+debug = True
+
+
+pid = PID(Kp=0.2, Ki=0.01, Kd=0.0, setpoint=0)#, output_limits=(0,100)#, sample_time=0.01,)
+
+rangeMax = 10000
+current_value = 0
+output = []
+target = []
+
+for i in range(0,rangeMax,1):
+	output.append(pid(current_value))
+	current_value = output[i]
+	if i == 1000:
+		pid.setpoint = 35
+	elif i == 6000:
+		pid.setpoint = 10
+	target.append(pid.setpoint)
+	if debug == True:
+		print("set point: " + str(pid.setpoint))
+		print("current value: " + str(current_value))
+		print("output value: " + str(output[i]))
+
+xpoints = range(0,rangeMax,1)
+
+plt.plot(xpoints, output)
+plt.plot(xpoints, target)
+plt.show()
+
+# for i in range(0, rangeMax - 1, 1):
+# 	output[i] = PID(current_value[i])
+# 	current_value[i] = output[i]
+# 	if i == 150:
+# 		pid.setpoint = 10
+
+# xpoints = range(0 , rangeMax , 1)
+# plt.plot(xpoints, output)
+# plt.show()
