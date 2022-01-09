00acc8e4f5037e3809e7c69401bfd6c3702bc830
ted <ted@roguebuckmechanical.com>
2022-01-08 17:02:14 -0800

    practice

--- py-garden.py
+++ py-garden.py
@@ -1,18 +1,38 @@
-class io:
-	pass
+
 
 class plant:
-	def __init__(self, plantName, plantType, waterDuty = 0.5) -> None:
+	def __init__(self, plantName, plantType, waterDuty = 0.5):
 		self.name = plantName
 		self.type = plantType
 		self.waterDuty = waterDuty
 
-	def set_name(self, plantName):
+	def setName(self, plantName):
 		self.name = plantName
 
-	def set_type(self, plantType ):
+	def setType(self, plantType ):
 		self.type = plantType
+	
+	def setMoistureIO(self, source):
+		self.moistureSource = source
 
-	def check_moisture(self):
-		if self.moisture == True
-		self.moisture = True
+	def checkMoisture(self):
+		try:
+			if self.moistureSource != None:
+				if self.moistureSource == True:
+					self.moisture = True
+				else:
+					self.moisture = False
+			return self.moisture
+		except:
+			print("No Moisture sensor found. Set moisture source with setMoistureIO()")
+
+	def printStatus(self):
+		print("Name:", self.name)
+		print("Type:", self.type)
+		print("Moisture:", self.checkMoisture())
+
+
+plant1 = plant("pep1" , "Jalapeno")
+#plant1.setMoistureIO(True)
+
+plant1.printStatus()
